02a039baff0a5af1b68e14c5da2283c5ca533c11
Taruna <tarunarawat2218@gmail.com>
2023-01-01 19:51:50 +0530

    add tailwind

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import './App.css';
 
 function App() {
   return (
-    <h1 className="head">Let start react</h1>
+    <h1 className="text-3xl">Let start react</h1>
+     
     );
-  }
+  }  
   
   export default App;
   
