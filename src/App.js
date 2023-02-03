90d4fb0123ab990ea9cd5c678d935245e2d3fdc2
Taruna <tarunarawat2218@gmail.com>
2023-02-03 18:41:03 +0530

    add components

--- src/App.js
+++ src/App.js
@@ -5,11 +5,15 @@ import Navbar from './components/Navbar';
 function App() {
   return (
     <>
-  <Navbar title = "Food Snap" />
+  <Navbar title = "Food Snap"    />
 </>
     );
   } 
    
   export default App;
+
+  // proptype and prop default
   
-  
+  Navbar.protypes = {
+    
+  }
