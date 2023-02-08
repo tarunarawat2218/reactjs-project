567b831cda4d10b55739b576b9c27e9cec427b31
Taruna <tarunarawat2218@gmail.com>
2023-02-08 22:38:20 +0530

    commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -26,21 +26,21 @@ export default function Navbar(props) {
       </a>
     </div>
     
-    <div class="flex justify-left">
+    {/* <div class="flex justify-left">
   <div class="mb-1 xl:w-96">
     <div class="input-group relative flex flex-wrap items-stretch w-full mb-0.2">
       <input type="search" class="form-control relative flex-auto min-w-0 block w-full px-3 py-1 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" placeholder="Search" aria-label="Search" aria-describedby="button-addon3"/> 
-      {/* <button class="btn inline-block px-6 py-2 border-2 border-blue-600 text-blue-600 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out" type="button" id="button-addon3">Search</button> */}
+       <button class="btn inline-block px-6 py-2 border-2 border-blue-600 text-blue-600 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out" type="button" id="button-addon3">Search</button> 
     </div>
-  </div>
+  </div> */}
 </div>
-{/* <label class="relative inline-flex items-center cursor-pointer">
+<label class="relative inline-flex items-center mb-5 cursor-pointer">
   <input type="checkbox" value="" class="sr-only peer"/>
-  <div class="w-9 h-5 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-  <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">Toggle me</span>
-</label> */}
+  <div class="w-9 h-5 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+  <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">ON/OFF</span>
+</label>
+ 
 
-</div>
 
   
 </nav>
