4ad2b6dd08c8ada2674c5599039557e2caae4d77
Taruna Rawat <86361519+tarunarawat2218@users.noreply.github.com>
2023-02-22 23:51:58 +0530

    posts

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -2,8 +2,10 @@ import React from 'react'
 
 export default function Posts() {
   return (
-    <>
-    <div class="max-w-sm rounded overflow-hidden shadow-lg px-2 my-5">
+  <>
+  <h1 className='text-5xl '>POSTS</h1>
+  <div className=' grid grid-cols-3 place-content-start'>
+  <div class="max-w-sm rounded overflow-hidden shadow-lg mx-5 my-5 p-3">
   <img class="w-full" src="/secondphoto (2).jpg" alt="Sunset in the mountains"/>
   <div class="px-6 py-4">
     <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
@@ -11,24 +13,16 @@ export default function Posts() {
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
     </p>
   </div>
-  <div class="px-6 pt-4 pb-2">
-    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">1 hour ago</span>
-   </div>
+  <div class="flex items-center">
+      <img class="w-10 h-10 rounded-full mr-4" src="/blogimage.jpg" alt="Avatar of Jonathan Reinink"/>
+      <div class="text-sm">
+        <p class="text-gray-900 leading-none">Jonathan Reinink</p>
+        <p class="text-gray-600">Aug 18</p>
+      </div>
 </div>
-<div class="max-w-sm rounded overflow-hidden  shadow-lg flex flex-wrap">
- <img class="w-full" src="/secondphoto (2).jpg" alt="Sunset in the mountains"/>
- <div class="px-6 py-4">
-   <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
-   <p class="text-gray-700 text-base">
-     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
-   </p>
- </div>
- <div class="px-6 pt-4 pb-2">
-    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">1 hour ago</span>
-   </div>
- 
-</div>
-<div class="max-w-sm rounded overflow-hidden shadow-lg px-2 my-5">
+  </div> 
+  
+  <div class="max-w-sm rounded overflow-hidden shadow-lg mx-5 my-5 p-3">
   <img class="w-full" src="/secondphoto (2).jpg" alt="Sunset in the mountains"/>
   <div class="px-6 py-4">
     <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
@@ -36,12 +30,48 @@ export default function Posts() {
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
     </p>
   </div>
-  <div class="px-6 pt-4 pb-2">
-    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">1 hour ago</span>
-   </div>
-
+  <div class="flex items-center">
+      <img class="w-10 h-10 rounded-full mr-4" src="/blogimage.jpg" alt="Avatar of Jonathan Reinink"/>
+      <div class="text-sm">
+        <p class="text-gray-900 leading-none">Jonathan Reinink</p>
+        <p class="text-gray-600">Aug 18</p>
+      </div>
 </div>
-</>
-
+  </div> 
+  
+  <div class="max-w-sm rounded overflow-hidden shadow-lg mx-5 my-5 p-3 ">
+  <img class="w-full" src="/secondphoto (2).jpg" alt="Sunset in the mountains"/>
+  <div class="px-6 py-4">
+    <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+    <p class="text-gray-700 text-base">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+    </p>
+  </div>
+  <div class="flex items-center">
+      <img class="w-10 h-10 rounded-full mr-4" src="/blogimage.jpg" alt="Avatar of Jonathan Reinink"/>
+      <div class="text-sm">
+        <p class="text-gray-900 leading-none">Jonathan Reinink</p>
+        <p class="text-gray-600">Aug 18</p>
+      </div>
+</div>
+  </div> 
+  <div class="max-w-sm rounded overflow-hidden shadow-lg mx-5 my-5 p-3 ">
+  <img class="w-full" src="/secondphoto (2).jpg" alt="Sunset in the mountains"/>
+  <div class="px-6 py-4">
+    <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+    <p class="text-gray-700 text-base">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+    </p>
+  </div>
+  <div class="flex items-center">
+      <img class="w-10 h-10 rounded-full mr-4" src="/blogimage.jpg" alt="Avatar of Jonathan Reinink"/>
+      <div class="text-sm">
+        <p class="text-gray-900 leading-none">Jonathan Reinink</p>
+        <p class="text-gray-600">Aug 18</p>
+      </div>
+</div>
+  </div> 
+  </div>
+  </>
   )
 }
