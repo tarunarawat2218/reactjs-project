aab907b389c97fb84c42924e4f6fdcfbf0ce988e
Taruna Rawat <86361519+tarunarawat2218@users.noreply.github.com>
2023-03-06 22:49:45 +0530

    delete files

--- src/App.js
+++ src/App.js
@@ -1,72 +1,60 @@
-import './App.css';
-import Navbar from './components/Navbar';
-import Login from './components/Login';
-import Textarea from './components/Textarea';
-import About from './components/About';
-import Form from './components/Form';
-import Edit from './components/Edit';
-import Firstpage from './components/Firstpage';
-import Posts from './components/Posts';
-import {useState } from 'react';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  
-} from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Login from "./components/Login";
+import Textarea from "./components/Textarea";
+import About from "./components/About";
+import Form from "./components/Form";
+import Edit from "./components/Edit";
+import Firstpage from "./components/Firstpage";
+import Posts from "./components/Posts";
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
- function App() {
-//   const [mode , setMode] = useState('bg-slate-50')
-//   const [alert , setAlert] = useState(null)
+function App() {
+  //   const [mode , setMode] = useState('bg-slate-50')
+  //   const [alert , setAlert] = useState(null)
 
-//   const showAlert = (message,type) => {
-//     setAlert({
-//       msg:message,
-//       type : type
-//     })
-//     setTimeout(() => {
-//       setAlert(null)
-//     }, 2000);
-//   }
+  //   const showAlert = (message,type) => {
+  //     setAlert({
+  //       msg:message,
+  //       type : type
+  //     })
+  //     setTimeout(() => {
+  //       setAlert(null)
+  //     }, 2000);
+  //   }
 
-//   const toggleMode = () =>{
-//     if(mode === 'bg-slate-50'){
-//       setMode( "bg-stone-900")
-//       showAlert("Dark mode" , "bg-red-700")
-      
-//     }else{
-//       setMode("bg-slate-50")
-//       showAlert("light mode" , "bg-blue-200")
-//     }
-//   }
+  //   const toggleMode = () =>{
+  //     if(mode === 'bg-slate-50'){
+  //       setMode( "bg-stone-900")
+  //       showAlert("Dark mode" , "bg-red-700")
+
+  //     }else{
+  //       setMode("bg-slate-50")
+  //       showAlert("light mode" , "bg-blue-200")
+  //     }
+  //   }
   return (
     <>
-    <Router>
-
-  <Navbar/>
-  <Routes>
-          <Route path="/" element={<Firstpage />} />
-           
-        </Routes>
-        
+      <Router>
+        <Navbar />
         <Routes>
-        <Route path="/Edit" element={<Edit/>} />
-        </Routes>  
-  <Routes>
+          <Route path="/" element={<Firstpage />} />
+        </Routes>
+
+        <Routes>
+          <Route path="/Edit" element={<Edit />} />
+        </Routes>
+        <Routes>
           <Route path="/about" element={<About />} />
-           
         </Routes>
         <Routes>
           <Route path="/textarea" element={<Textarea />} />
-           
         </Routes>
-  </Router>
-</>
-    );
-  
-  }
-  export default App;
+      </Router>
+    </>
+  );
+}
+export default App;
 
-  // proptype and prop default
-  
-  
+// proptype and prop default
--- src/components/About.js
+++ src/components/About.js
@@ -27,6 +27,5 @@ export default function About(props) {
 </div>
 </div>
 
-
-  );
+);
 }
